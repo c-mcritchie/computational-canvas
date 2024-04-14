1b76bd11356f16b211dc1d2256089f3f71306f1d
cmbackflip <colem1700@gmail.com>
2024-04-14 00:33:30 -0600

    fix run

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,12 @@
 import type {LayoutLoad} from "./$types";
 
+export const prerender = true;
+export const trailingSlash = 'always';
+
+export const config = {
+    runtime: 'nodejs18.x'
+}
+
 export const load : LayoutLoad = (async ({url : {pathname}}) => {
     return {
         pathname,
